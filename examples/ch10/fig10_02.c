6232f4fbe8a9591629f1470a7095c48392ca39a5
Manny Soto <mannysoto@yahoo.com>
2023-11-04 13:46:58 -0600

    removed unnecesary comments

--- examples/ch10/fig10_02.c
+++ examples/ch10/fig10_02.c
@@ -1,4 +1,3 @@
-// fig10_02.c
 // Card shuffling and dealing program using structures
 #include <stdio.h>
 #include <stdlib.h>
@@ -68,17 +67,3 @@ void deal(const Card * const deck) {
 }
 
 
-/**************************************************************************
- * (C) Copyright 1992-2021 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
