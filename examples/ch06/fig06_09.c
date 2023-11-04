89691a3e8bb79e8256d80c1dfcc9587688bd7a5b
Manny Soto <mannysoto@yahoo.com>
2023-11-04 12:29:04 -0600

    Removed unnecesary comments

--- examples/ch06/fig06_09.c
+++ examples/ch06/fig06_09.c
@@ -1,4 +1,3 @@
-// fig06_09.c
 // Static arrays are initialized to zero if not explicitly initialized.
 #include <stdio.h>
 
@@ -58,18 +57,4 @@ void automaticArrayInit(void) {
 } 
 
 
-/**************************************************************************
- * (C) Copyright 1992-2021 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
 
--- examples/ch06/fig06_18.c
+++ examples/ch06/fig06_18.c
@@ -1,4 +1,3 @@
-// fig06_18.c
 // Using variable-length arrays in C99
 #include <stdio.h>
 
@@ -79,18 +78,4 @@ void print2DArray(size_t row, size_t col, int array[row][col]) {
    } 
 } 
 
-/**************************************************************************
- * (C) Copyright 1992-2021 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
 
