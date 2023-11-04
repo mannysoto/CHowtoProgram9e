4dd60e72bea7d1d164a59c204be36cf761a810b8
Manny Soto <mannysoto@yahoo.com>
2023-11-04 12:22:52 -0600

    Removed unncesary comments

--- examples/ch05/ex05_03.c
+++ examples/ch05/ex05_03.c
@@ -1,4 +1,3 @@
-// ex05_03.c
 // Testing the math library functions
 #include <stdio.h>
 #include <math.h>
@@ -56,17 +55,4 @@ int main(void) {
    printf("tan(%.1f) = %.1f\n", 0.0, tan(0.0));
 } 
 
-/**************************************************************************
- * (C) Copyright 1992-2021 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
+
