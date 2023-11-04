d47dd19218a6fd096aef54d6989a6fd64fe97315
Manny Soto <mannysoto@yahoo.com>
2023-11-04 12:16:34 -0600

    removed extra and unnecessary comments

--- examples/ch04/fig04_02.c
+++ examples/ch04/fig04_02.c
@@ -1,4 +1,3 @@
-// fig04_02.c
 // Counter-controlled iteration with the for statement.
 #include <stdio.h>
 
@@ -12,19 +11,4 @@ int main(void) {
    puts(""); // outputs a newline
 }
 
-
-/**************************************************************************
- * (C) Copyright 1992-2015 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
 
--- examples/ch04/fig04_07.c
+++ examples/ch04/fig04_07.c
@@ -1,4 +1,3 @@
-// fig04_07.c
 // Using the break statement in a for statement.
 #include <stdio.h>
 
@@ -18,19 +17,4 @@ int main(void) {
    printf("\nBroke out of loop at x == %d\n", x);
 }
 
-
-/**************************************************************************
- * (C) Copyright 1992-2015 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
 
